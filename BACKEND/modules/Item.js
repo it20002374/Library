77e7e2013b9854d6925c38f58f31bdac77b77c06
sonal-uchiru <sonal123326@gmail.com>
2022-03-27 22:07:29 +0530

    Test

---
+++ BACKEND/modules/Item.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    Title : {
+        type : String,
+        required : true
+    },
+
+    SubTitle : {
+        type : String,
+        required : true
+    },
+
+    Author : {
+        type : String,
+        required : true
+    },
+
+    Quantity : {
+        type : String,
+        required : true
+    },
+
+    Price : {
+        type : String,
+        required : true
+    },
+
+    Description : {
+            type : String,
+            required : true
+    },
+
+    // Images : [{
+    //     type : String,
+    // }],
+
+    Category : {
+        type : String,
+        required :true,
+        default : "Others"
+    },
+
+    ItemAvailabilityStatus : {
+        type : Boolean,
+        required : true,
+        default : true
+    },
+
+    StaffID : {
+        type : String,
+        required : false,
+    },
+
+    CustomerID : {
+        type : String,
+        required : false,
+    },
+
+    Date : {
+        type : String,
+        required : true,
+    },
+
+    AddedDate : {
+        type : Date,
+        required: true,
+        default : new Date()
+    }
+
+})
+
+const Item = mongoose.model("Item",ItemSchema);
+module.exports = Item;
