4e6b5ddbdb7aa2cddd964b50c9250cc66611d600
TharinduDeshan <tharindudeshan50@gmail.com>
2022-04-01 00:39:03 +0530

    commit7

--- frontend/src/components/items/editItems.js
+++ frontend/src/components/items/editItems.js
@@ -107,11 +107,17 @@ export default function EditItems(){
     //     newdata[e.target.id] = e.target.value;
     //     setData(newdata);
 
-      //   function handle(e) {
+     //   function handle(e) {
     //     const newdata = { ...data };
     //     newdata[e.target.id] = e.target.value;
     //     setData(newdata);
 
+     //   function handle(e) {
+    //     const newdata = { ...data };
+    //     newdata[e.target.id] = e.target.value;
+    //     setData(newdata);
+
+ 
 
     return(
 
