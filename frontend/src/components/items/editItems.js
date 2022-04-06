03a8e53e59825c138637910eb5a6809ea697d711
anjalee98 <anjaleeambegoda5@gmail.com>
2022-04-06 16:34:03 +0530

    comment1

--- frontend/src/components/items/editItems.js
+++ frontend/src/components/items/editItems.js
@@ -15,10 +15,7 @@ export default function EditItems(){
   let flag = 0;
 
   const [data, setData] = useState({
-    // Quantity: "",
-    // Price: "",
-    // Category: "",
-    // Description: "",
+   
   });
 
 
